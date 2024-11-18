jstrieb/code-grid

commit 96fb1ab47072e89dd6306613877b17f5b35a7183

Move more logic into classes and out of HTML

src/classes.svelte.js
@@ -22,6 +22,9 @@ export class Sheet {
   }
 
   addRows(n, start = this.heights.length) {
+    if (n < 0) {
+      return this.deleteRows(-n, start);
+    }
     this.heights.splice(start, 0, ...new Array(n).fill(DEFAULT_HEIGHT));
     this.cells.splice(
       start,
@@ -40,6 +43,9 @@ export class Sheet {
   }
 
   addCols(n, start = this.widths.length) {
+    if (n < 0) {
+      return this.deleteCols(-n, start);
+    }
     this.widths.splice(start, 0, ...new Array(n).fill(DEFAULT_WIDTH));
     this.cells.map((row) =>
       row.splice(
@@ -53,6 +59,26 @@ export class Sheet {
   deleteCols(n, start = this.widths.length - n) {
     this.widths.splice(start, n);
     this.cells.map((row) => row.splice(start, n));
+  }
+
+  autoResizeCol(i) {
+    const newWidth = Math.max(
+      ...this.cells
+        .map((row) => row[i])
+        .map((cell) => cell.naturalSize().width),
+    );
+    if (!Number.isNaN(newWidth)) {
+      this.widths[i] = newWidth;
+    }
+  }
+
+  autoResizeRow(i) {
+    const newHeight = Math.max(
+      ...this.cells[i].map((cell) => cell.naturalSize().height),
+    );
+    if (!Number.isNaN(newHeight)) {
+      this.heights[i] = newHeight;
+    }
   }
 }
 
@@ -141,5 +167,13 @@ export class Selection {
 
   contains(i) {
     return this.min <= i && i <= this.max;
+  }
+
+  row(i) {
+    return this.type == "row" && this.contains(i);
+  }
+
+  col(i) {
+    return this.type == "col" && this.contains(i);
   }
 }

src/Table.svelte
@@ -179,32 +179,32 @@
     };
   }
 
-  function isRowSelected(i) {
-    return selected.type == "row" && selected.contains(i);
-  }
-
-  function isColSelected(i) {
-    return selected.type == "col" && selected.contains(i);
-  }
-
-  function autoResizeCol(i) {
-    const newWidth = Math.max(
-      ...sheet.cells
-        .map((row) => row[i])
-        .map((cell) => cell.naturalSize().width),
-    );
-    if (!Number.isNaN(newWidth)) {
-      sheet.widths[i] = newWidth;
+  function colBoxShadow(i) {
+    if (!selected.col(i)) {
+      return;
     }
+    const result = ["inset 0 1px 0 0 var(--fg-color)"];
+    if (!selected.col(i - 1)) {
+      result.push("inset 1px 0 0 0 var(--fg-color)");
+    }
+    if (!selected.col(i + 1)) {
+      result.push("inset -1px 0 0 0 var(--fg-color)");
+    }
+    return result.join(", ");
   }
 
-  function autoResizeRow(i) {
-    const newHeight = Math.max(
-      ...sheet.cells[i].map((cell) => cell.naturalSize().height),
-    );
-    if (!Number.isNaN(newHeight)) {
-      sheet.heights[i] = newHeight;
+  function rowBoxShadow(i) {
+    if (!selected.row(i)) {
+      return;
     }
+    const result = ["inset 1px 0 0 0 var(--fg-color)"];
+    if (!selected.row(i - 1)) {
+      result.push("inset 0 1px 0 0 var(--fg-color)");
+    }
+    if (!selected.row(i + 1)) {
+      result.push("inset 0 -1px 0 0 var(--fg-color)");
+    }
+    return result.join(", ");
   }
 </script>
 
@@ -217,16 +217,8 @@
           {@const pointermoveHandler = pointermoveX(i)}
           <th
             style:--width="{width}px"
-            class:selected={isColSelected(i)}
-            style:box-shadow={[
-              ...(isColSelected(i) ? ["inset 0 1px 0 0 var(--fg-color)"] : []),
-              ...(isColSelected(i) && !isColSelected(i - 1)
-                ? ["inset 1px 0 0 0 var(--fg-color)"]
-                : []),
-              ...(isColSelected(i) && !isColSelected(i + 1)
-                ? ["inset -1px 0 0 0 var(--fg-color)"]
-                : []),
-            ].join(", ")}
+            class:selected={selected.col(i)}
+            style:box-shadow={colBoxShadow(i)}
           >
             <div class="header">
               <button
@@ -255,12 +247,12 @@
               onpointerdown={pointerdown(pointermoveHandler)}
               onpointerup={pointerup(pointermoveHandler)}
               ondblclick={() => {
-                if (selected.type == "col" && selected.contains(i)) {
+                if (selected.col(i)) {
                   for (let j = selected.min; j <= selected.max; j++) {
-                    autoResizeCol(j);
+                    sheet.autoResizeCol(j);
                   }
                 } else {
-                  autoResizeCol(i);
+                  sheet.autoResizeCol(i);
                 }
               }}
               class="right handle"
@@ -277,16 +269,8 @@
         <tr>
           <th
             style:--height="{sheet.heights[i]}px"
-            class:selected={isRowSelected(i)}
-            style:box-shadow={[
-              ...(isRowSelected(i) ? ["inset 1px 0 0 0 var(--fg-color)"] : []),
-              ...(isRowSelected(i) && !isRowSelected(i - 1)
-                ? ["inset 0 1px 0 0 var(--fg-color)"]
-                : []),
-              ...(isRowSelected(i) && !isRowSelected(i + 1)
-                ? ["inset 0 -1px 0 0 var(--fg-color)"]
-                : []),
-            ].join(", ")}
+            class:selected={selected.row(i)}
+            style:box-shadow={rowBoxShadow(i)}
           >
             <div class="header">
               <button
@@ -315,12 +299,12 @@
               onpointerdown={pointerdown(pointermoveHandler)}
               onpointerup={pointerup(pointermoveHandler)}
               ondblclick={() => {
-                if (selected.type == "row" && selected.contains(i)) {
+                if (selected.row(i)) {
                   for (let j = selected.min; j <= selected.max; j++) {
-                    autoResizeRow(j);
+                    sheet.autoResizeRow(j);
                   }
                 } else {
-                  autoResizeRow(i);
+                  sheet.autoResizeRow(i);
                 }
               }}
               class="bottom handle"
@@ -347,13 +331,7 @@
     <Button
       style="width: 100%;"
       disabled={!Number.isInteger(toAdd) || toAdd == 0}
-      onclick={() => {
-        if (toAdd > 0) {
-          sheet.addCols(toAdd);
-        } else if (toAdd < 0) {
-          sheet.deleteCols(-toAdd);
-        }
-      }}
+      onclick={() => sheet.addCols(toAdd)}
       >{#if toAdd >= 0}Add {toAdd}{:else}Delete {-toAdd}{/if} column{#if Math.abs(toAdd) != 1}s{/if}</Button
     >
   </div>
@@ -361,24 +339,16 @@
   <div class="add rows">
     <Button
       disabled={!Number.isInteger(toAdd) || toAdd == 0}
-      onclick={() => {
-        if (toAdd > 0) {
-          sheet.addRows(toAdd);
-        } else if (toAdd < 0) {
-          sheet.deleteRows(-toAdd);
-        }
-      }}
+      onclick={() => sheet.addRows(toAdd)}
       >{#if toAdd >= 0}Add {toAdd}{:else}Delete {-toAdd}{/if} row{#if Math.abs(toAdd) != 1}s{/if}</Button
     >
   </div>
 
   <div class="numberinput">
     <NumericInput
-      style="width: 100%; text-align: center; max-width: 5ch;"
+      style="width: 100%; text-align: center; max-width: 7ch;"
       bind:value={toAdd}
-      onfocus={(e) => {
-        e.target.select();
-      }}
+      onfocus={(e) => e.target.select()}
     />
   </div>
 </div>

commit dded9f731612afc15ca6856af5f6e2e64a3ba002

Fix delete{Rows,Cols} bug due to default arguments
Bug introduced in 96fb1ab47072e89dd6306613877b17f5b35a7183.

src/classes.svelte.js
@@ -23,7 +23,9 @@ export class Sheet {
 
   addRows(n, start = this.heights.length) {
     if (n < 0) {
-      return this.deleteRows(-n, start);
+      // Explicitly use the arguments object to avoid using the default value if
+      // no start is set
+      return this.deleteRows(-n, arguments[1]);
     }
     this.heights.splice(start, 0, ...new Array(n).fill(DEFAULT_HEIGHT));
     this.cells.splice(
@@ -44,7 +46,9 @@ export class Sheet {
 
   addCols(n, start = this.widths.length) {
     if (n < 0) {
-      return this.deleteCols(-n, start);
+      // Explicitly use the arguments object to avoid using the default value if
+      // no start is set
+      return this.deleteCols(-n, arguments[1]);
     }
     this.widths.splice(start, 0, ...new Array(n).fill(DEFAULT_WIDTH));
     this.cells.map((row) =>