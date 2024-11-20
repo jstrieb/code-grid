465f0a4ee1ac331f7516284ef5a46eb1375e8023
Jacob Strieb <jstrieb@users.noreply.github.com>
2024-11-20 02:30:53 -0500

    Add draggable dialog component

---
+++ src/Dialog.svelte
@@ -0,0 +1,142 @@
+<style>
+  /* The [open] is required for the .close() method to work. */
+  dialog[open] {
+    border: 1px solid var(--fg-color);
+    box-shadow: 4px 4px 0 0 var(--fg-color);
+    padding: 0.25em;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: stretch;
+    width: 80ch;
+    height: 15em;
+    gap: 0.5em;
+  }
+
+  .top {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: stretch;
+    gap: 0.25em;
+  }
+
+  .top .drag {
+    flex-grow: 1;
+    cursor: grab;
+    height: 1.5em;
+    touch-action: none;
+    border: 1px solid var(--fg-color);
+  }
+
+  .top .drag:active {
+    cursor: grabbing;
+  }
+
+  button {
+    cursor: pointer;
+    font-family: monospace, monospace;
+    width: 1.5em;
+    height: 1.5em;
+    border: 1px solid var(--fg-color);
+    box-shadow: 2px 2px 0 0 var(--fg-color);
+    margin-right: 2px;
+  }
+
+  button:hover {
+    outline: 1px solid var(--fg-color);
+  }
+
+  button:active {
+    box-shadow: 1px 1px 0 0 var(--fg-color);
+  }
+
+  .main {
+    flex-grow: 1;
+  }
+</style>
+
+<script>
+  let {
+    children,
+    top = $bindable(50),
+    left = $bindable(50),
+    ...rest
+  } = $props();
+  let dialog = $state();
+  let pointerStart = $state();
+
+  function pointerdown(e) {
+    e.target.addEventListener("pointermove", pointermove);
+    e.target.setPointerCapture(e.pointerId);
+    pointerStart = { x: e.clientX, y: e.clientY };
+  }
+
+  function pointerup(e) {
+    e.target.removeEventListener("pointermove", pointermove);
+    e.target.releasePointerCapture(e.pointerId);
+  }
+
+  function pointermove(e) {
+    const dx = e.clientX - pointerStart.x;
+    left += dx;
+    const dy = e.clientY - pointerStart.y;
+    top += dy;
+    pointerStart.x = e.clientX;
+    pointerStart.y = e.clientY;
+  }
+
+  function open(e) {
+    e.show();
+  }
+</script>
+
+<dialog
+  bind:this={dialog}
+  use:open
+  style:top="{top}px"
+  style:left="{left}px"
+  {...rest}
+>
+  <div class="top">
+    <svg class="drag" onpointerdown={pointerdown} onpointerup={pointerup}>
+      <defs>
+        <pattern
+          id="dots"
+          x="0"
+          y="0"
+          width="6"
+          height="9"
+          viewBox="0 0 6 9"
+          patternUnits="userSpaceOnUse"
+        >
+          <rect x="1" y="0" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+          <rect x="1" y="3" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+          <rect x="1" y="6" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+          <rect x="4" y="2" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+          <rect x="4" y="5" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+          <rect x="4" y="8" width="1" height="1" style="fill: var(--fg-color);"
+          ></rect>
+        </pattern>
+      </defs>
+      <rect
+        width="100%"
+        height="100%"
+        fill="url(#dots)"
+        style="stroke: var(--bg-color); stroke-width: 2px;"
+      ></rect>
+    </svg>
+    <button onclick={() => dialog.close()}>X</button>
+  </div>
+  <div class="main">
+    {@render children()}
+  </div>
+</dialog>
